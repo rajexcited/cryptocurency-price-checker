db6c9d43cc37e140265c2a4e95f7b710e4718015
raj <rajexcited@gmail.com>
2018-01-18 02:40:53 -0500

    error handling and access logs

--- app.js
+++ app.js
@@ -4,22 +4,29 @@ console.time('server-startup');
 var app,
     express = require('express'),
     cors = require('cors'),
-    errorhandler = require('errorhandler');
+    morgan = require('morgan'), 
+    isDevelopment = (process.env.NODE_ENV === 'development');
 
+console.log('node env', process.env.NODE_ENV);
 // Create global app object
 app = express();
 app.use(cors());
-
+app.use(morgan('combined'));
 /*******
   Routes can be added here.
   ******/
 /// START-error handlers
-app.use(errorhandler());
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+app.use(function(err, req, res, next) {
+  var statusCode = err.status || 500,
+      msg = isDevelopment ? err.stack : 'Something broke!';
+  console.error(err.stack);
+  res.status(statusCode).send(msg);
 });
 /// END-error handlers
 
