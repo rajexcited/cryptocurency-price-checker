56019c22a159e6be63bbc03fc3b0f1ea2099fdd7
raj <rajexcited@gmail.com>
2018-01-22 21:22:54 -0500

    formatting

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,6 @@
 var router = require('express').Router({
-		mergeParams: true
+		mergeParams: true,
+		strict: true
 	}),
 	producer = require('../../lib/producer-xml-json'),
 	diffFinder = require('./difference-finder');
